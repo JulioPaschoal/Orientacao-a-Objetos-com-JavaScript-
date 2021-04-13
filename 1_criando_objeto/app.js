74f17c473911ec2e6bbdd631f7bd670eb23c577d
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2021-04-12 22:16:57 -0300

    Iniciando codigos em javscript

---
+++ 1_criando_objeto/app.js
@@ -0,0 +1,13 @@
+let carro = {
+    portas: 4,
+    cor: "Vermelha"
+}
+
+console.log(carro);
+console.log(typeof carro);
+
+let pessoa = {
+    nome: "Julio Paschoal"
+}
+
+console.log(pessoa)
---
+++ 2_propiedades/app.js
@@ -0,0 +1,16 @@
+let carro = {
+  portas: 4,
+  cor: "Vermelha",
+};
+
+console.log(carro.portas);
+console.log(carro.cor);
+
+if (carro.portas > 2) {
+    console.log("Este carro tem mais de duas portas!");
+}
+let pessoa = {
+  nome: "Julio Paschoal",
+};
+
+console.log(pessoa['nome'])
